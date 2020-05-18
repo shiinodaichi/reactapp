001d37dc8a94ed3a4a6e748e9558ef806e37999a
dshiino <shiinodaichi@gmail.com>
2020-05-18 20:25:09 +0900

    ディレクトリ移動

--- react/reactapp/src/components/Api.jsx
+++
@@ -1,7 +0,0 @@
-// import axios from 'axios';
-
-// const getDataFromAPI = async keyword =>{
-//     const requestUrl = 'https://www.google.apis.com/books/v1/volumes?q=intitle:';
-//     const result = await axios.get(`${requestUrl}${keyword}`);
-//     return result;
-// }
